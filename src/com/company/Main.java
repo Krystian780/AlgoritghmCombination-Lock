84567eaff0babf1e2a5ef2b5ee3199e3f55f5d62
Krystian M <krystian2007@gmail.com>
2022-09-08 22:05:58 +0200

    fourth solved

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,55 +3,30 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("change allowing creating pull request");
-	int digits = 5;
-	int [] currentLock = new int[]{8,2,1,9,5};
-	int [] correctCode = new int[]{6,4,7,2,3};
-        System.out.println(get(digits, currentLock, correctCode));
+        System.out.println(getAmountOfCalls(9999));
     }
 
-    public static int get(int digits, int[] currentLock, int[] correctCode){
-        int howManyChanges = 0;
-        for(int x = 0; x < digits; x++){
-            int correctCode2 = correctCode[x];
-            int startingPointLooping = currentLock[x];
-            int leftCountTillFound = 0;
-            int rightCountTillFound = 0;
-            boolean whenFoundIteratingtoTheRightSetToFalse = true;
-            while(whenFoundIteratingtoTheRightSetToFalse){
-                if(startingPointLooping==correctCode2){
-                    whenFoundIteratingtoTheRightSetToFalse = false;
-                }
-                else{
-                    if(startingPointLooping==10){
-                        startingPointLooping = 1;
-                        rightCountTillFound++;
-                    }else{
-                        startingPointLooping++;
-                        rightCountTillFound++;
-                    }
-                }
-            }
-            startingPointLooping = currentLock[x];
-            boolean leftIterating = true;
-            while(leftIterating){
-                if(startingPointLooping==correctCode2){
-                    leftIterating = false;
-                }
-                else{
-                    if(startingPointLooping==0){
-                        startingPointLooping = 10;
-                        leftCountTillFound++;
-                    }else{
-                        startingPointLooping--;
-                        leftCountTillFound++;
-                    }
-                }
-            }
-            howManyChanges =  howManyChanges + (Math.min(leftCountTillFound,rightCountTillFound));
+    public static int getAmountOfCalls(int digit){
+        int length = String.valueOf(digit).length();
+        int howManyTimes = 0;
+        int rest = 0;
+        if(length==1){
+            howManyTimes = digit-1;
+        }if(length>1){
+            char digits = (Integer.toString(digit).charAt(0));
+            howManyTimes = Character.getNumericValue(digits) -1;
         }
 
-        return howManyChanges-1;
-    }
+        if(length==1){
+            rest = 1;
+        }else if(length==2){
+            rest = 3;
+        }else if(length==3){
+            rest = 6;
+        }else{
+            rest = 10;
+        }
 
+        return howManyTimes * 10 + rest;
+    }
 }
